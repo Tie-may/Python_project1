6c1b6787012cecb0b810d89ff7207fa1b3f68715
tie_may <zhangxiang2233@gmail.com>
2021-03-19 08:33:58 +0800

    优化账号密码错误提示

--- static/js/login.js
+++ static/js/login.js
@@ -1,15 +1,33 @@
 $(function () {
-    var isSuccess = true;
-    $("#submit").click(function (e) {
-        if (!isSuccess) {
-            e.preventDefault();
-            alert('输入的用户名或密码有误')
+    var isSuccess;
+    var isPrevent = true;
+    $('#submit').click(function (e) {
+        isSuccess = $('body').attr("data-flag");
+        if (isSuccess === "False") {
+            if(isPrevent)e.preventDefault();
+            $('#error_psw01').html(
+                "<svg class='icon' aria-hidden='true'>" +
+                "<use xlink:href='#icon-chahao'></use>" +
+                "</svg>" +
+                "用户名或密码输入错误"
+            );
         }
     });
-    $("#btn_modify").click(function (e) {
-        if (!isSuccess) {
-            e.preventDefault();
-            alert('输入的用户名或密码有误')
+
+    $('#btn_modify').click(function (e) {
+        isSuccess = $('body').attr("data-flag");
+        if (isSuccess === "False") {
+            if(isPrevent)e.preventDefault();
+            $('#error_psw02').html(
+                "<svg class='icon' aria-hidden='true'>" +
+                "<use xlink:href='#icon-chahao'></use>" +
+                "</svg>" +
+                "用户名或密码输入错误"
+            );
         }
+    });
+
+    $(".entering").focus(function (){
+        isPrevent = false;
     });
 })
