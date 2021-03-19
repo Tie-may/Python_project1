c93da1ea8608cfee3cc72f1f0ae60b09ad4ab8c4
tie_may <zhangxiang2233@gmail.com>
2021-03-19 10:05:27 +0800

    优化账号密码错误提示

--- static/js/login.js
+++ static/js/login.js
@@ -1,10 +1,11 @@
 $(function () {
-    var isSuccess;
-    var isPrevent = true;
-    $('#submit').click(function (e) {
+    var isSuccess; // 是否登陆成功（上一次）
+    var isPrevent = true; // 是否阻止提交按钮默认行为
+
+    $("#submit").on('click', function (e) {
         isSuccess = $('body').attr("data-flag");
         if (isSuccess === "False") {
-            if(isPrevent){
+            if (isPrevent) {
                 e.preventDefault();
                 $("#submit").addClass("disabled");
             }
@@ -12,30 +13,26 @@ $(function () {
                 "<svg class='icon' aria-hidden='true'>" +
                 "<use xlink:href='#icon-chahao'></use>" +
                 "</svg>" +
-                "用户名或密码输入错误"
+                "用户名密码输入错误"
             );
         }
-    });
+    })
+    $("#submit").triggerHandler("click");
 
     $('#btn_modify').click(function (e) {
         isSuccess = $('body').attr("data-flag");
         if (isSuccess === "False") {
-            if(isPrevent){
-                e.preventDefault();
-                $("#btn_modify").addClass('disabled');
-            }
-            $('#error_psw02').html(
+            $('#error_psw01').html(
                 "<svg class='icon' aria-hidden='true'>" +
                 "<use xlink:href='#icon-chahao'></use>" +
                 "</svg>" +
-                "用户名或密码输入错误"
+                "修改密码失败！"
             );
         }
     });
 
-    $(".entering").focus(function (){
+    $(".entering").focus(function () {
         isPrevent = false;
         $('#submit').removeClass('disabled');
-        $('#btn_modify').removeClass('disabled');
     });
 })
